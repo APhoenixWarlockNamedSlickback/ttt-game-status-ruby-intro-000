d7c9067c9ac4ed04844c6fae7ebc8f0c98adf33e
Ted Swedalla <fluffingthevoid@gmail.com>
2018-01-07 21:52:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ top_row_win = [0,1,2]
 
 def won?(board)
   if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-    puts "#{win_index}"
+    puts "X won in the top row"
   else
     false
   end
