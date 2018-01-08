d9cd2f7c35615e58c9f7991adf3e2a43f18585d4
Ted Swedalla <fluffingthevoid@gmail.com>
2018-01-08 01:49:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,12 +36,8 @@ def full?(board)
 def draw?(board)
   if full?(board) == true && won?(board) == false
     return true
-#  elsif full?(board) == false && won?(board) == false
-#    return true
   else
     false
-#  elsif won?(board)
-#    false
   end
 end
 
